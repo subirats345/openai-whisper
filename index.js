2300d7170171af7e3ccd144e7a0035c59aa28785
subirats345 <subirats345@gmail.com>
2023-07-21 14:31:37 +0200

    changes

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
-require('dotenv').config();
+require("dotenv").config();
+const fs = require("fs");
+const express = require("express");
+const axios = require("axios");
+const multer = require("multer"); // para el manejo de archivos multipart/form-data
 
-const fs = require('fs');
-const axios = require('axios');
+const upload = multer({ dest: "uploads/" }); // guarda los archivos subidos en el directorio 'uploads/'
 
-async function transcribe(file) {
+const app = express();
+const port = 3000;
+
+async function transcribe(file, language = "en") {
   const response = await axios.post(
-    'https://api.openai.com/v1/audio/transcriptions',
+    "https://api.openai.com/v1/audio/transcriptions",
     {
       file,
-      model: 'whisper-1'
+      model: "whisper-1",
+      language: language,
     },
     {
       headers: {
-        'Content-Type': 'multipart/form-data',
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
-      }
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
     }
   );
 
   return response.data.text;
 }
 
-async function main() {
-  const file = fs.createReadStream('audio.mp3');
-  const transcript = await transcribe(file);
+app.post("/transcribe", upload.single("audio"), async (req, res) => {
+  const file = fs.createReadStream(req.file.path);
+  try {
+    const transcript = await transcribe(file);
+    res.json({ transcript: transcript });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Error al transcribir el archivo" });
+  } finally {
+    fs.unlinkSync(req.file.path); // Elimina el archivo después de usarlo
+  }
+});
 
-  console.log(transcript);
-}
-
-main();
+app.listen(port, () => {
+  console.log(
+    `API de transcripción de audio escuchando en http://localhost:${port}`
+  );
+});
